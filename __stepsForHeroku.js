3a768b51e824e49bed9485113f15f4c98ad515eb
ADIF KHAN <pkadif@gmail.com>
2022-12-30 19:32:23 +0600

    vercel deploy

---
+++ __stepsForHeroku.js
@@ -0,0 +1,14 @@
+/**
+ * ------------------------------
+ * only one time for a single pc
+ * ------------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ------------------------------
+ * one time for each project
+ * ------------------------------
+ * 1. heroku create 
+ * 
+ */
