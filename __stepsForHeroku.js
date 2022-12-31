05d6569a9aebdc99757feb1c77be2830661a9533
ADIF KHAN <pkadif@gmail.com>
2022-12-31 22:13:25 +0600

    heroku steps added

--- __stepsForHeroku.js
+++ __stepsForHeroku.js
@@ -10,5 +10,15 @@
  * one time for each project
  * ------------------------------
  * 1. heroku create 
- * 
+ * 2. push latest project to  github (for every changes)
+ * 3. git push heroku main (for every changes)
+ * 4. project>settings>reveal config vars
+ * 5. copy paste config vars from .env file
+ * 6. whitelist all ip address to access mongodb
+ * --------------------------------
+ * connect server with client & deploy
+ * --------------------------------
+ * 1. replace localhost5000 with  heroku link
+ * 2. npm run build (client site)
+ * 3. firebase deploy
  */
